1df4d2acb6d35a312807b125da6cd98e096c5ec7
Felix Brockmeier <felix.brockmeier@outlook.com>
2021-06-25 17:47:09 +0100

    minor fix

--- src/components/heatmap/heatmap.tsx
+++ src/components/heatmap/heatmap.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react';
 import Chart from 'react-apexcharts';
 
-export default (options: any, series: any, type: any): ReactElement => (
+export default (options: any, series: any[], type: any): ReactElement => (
   <Chart options={options} series={series} type={type} />
 );
