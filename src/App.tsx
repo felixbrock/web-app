5c7c12d26624ce9684c4b1ba7fdc60e1ac2255e8
Felix Brockmeier <felix.brockmeier@outlook.com>
2021-08-26 16:22:55 +0100

    rerouting and fixed auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import React, { ReactElement } from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import Amplify from 'aws-amplify';
+import React, { ReactElement, useState, useEffect } from 'react';
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+} from 'react-router-dom';
+import Amplify, { Auth } from 'aws-amplify';
 import TopNav from './components/top-nav/top-nav';
 // import Subscriptions from './components/subscriptions/subscriptions';
 // import Systems from './components/systems/systems';
@@ -42,26 +47,49 @@ export default (): ReactElement => {
     },
   });
 
-  return (
-    <div className="App">
-      <App>
-        <Router>
-          <HeaderContainer>
-            <TopNav />
-          </HeaderContainer>
-          <ContentContainer>
-            <Switch>
-              <Route path="/systems" component={Systems} />
-              <Route path="/system/:id" component={Selectors} />
-              <Route path="/automations" component={Automations} />
-              <Route path="/historical-data" component={HistoricalData} />
-            </Switch>
-          </ContentContainer>
-          <FooterContainer>
-            <Footer />
-          </FooterContainer>
-        </Router>
-      </App>
-    </div>
-  );
+  const [user, setUser] = useState();
+
+  const [app, setApp] = useState<ReactElement>(<div />);
+
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then((cognitoUser) => setUser(cognitoUser))
+      .catch((error) => {
+        console.log(error);
+
+        Auth.federatedSignIn();
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!user) return;
+
+    setApp(
+      <div className="App">
+        <App>
+          <Router>
+            <HeaderContainer>
+              <TopNav />
+            </HeaderContainer>
+            <ContentContainer>
+              <Switch>
+                <Route path="/systems" component={Systems} />
+                <Route path="/system/:id" component={Selectors} />
+                <Route path="/automations" component={Automations} />
+                <Route path="/historical-data" component={HistoricalData} />
+                <Route exact path="/">
+                  <Redirect to="/systems" />
+                </Route>
+              </Switch>
+            </ContentContainer>
+            <FooterContainer>
+              <Footer />
+            </FooterContainer>
+          </Router>
+        </App>
+      </div>
+    );
+  }, [user]);
+
+  return app;
 };
