5e503ad9eb41cc4205dffe7f3cf201ee5ab5cd5b
Felix Brockmeier <felix.brockmeier@outlook.com>
2021-10-12 18:28:02 +0200

    env message

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.log(`Running in ${process.env.REACT_APP_STAGE}`);
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App/>
